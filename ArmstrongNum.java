d3feb728829c3159e0f01abc0b481a356fdfe04d
Ashish Panda <37595903+Ashish-85@users.noreply.github.com>
2022-03-23 00:59:31 +0530

    Add files via upload

--- ArmstrongNum.java
+++ ArmstrongNum.java
@@ -4,12 +4,22 @@ public class ArmstrongNum {
 
 	public static void main(String[] args) {
 
-		int input = 153;
-		int calculated;
-		int remainder;
+		int input = 193;
+		int calculate = 0;
+		int rem;
 		int original;
-		
-		original = input;
-	}
 
+		original = input;
+
+		while (input > 0) {
+			rem = input % 10;
+			calculate = calculate + (rem * rem * rem);
+			input = input / 10;
+		}
+		if (calculate == original) {
+			System.out.println("The Number is an Armstrong Number");
+		} else {
+			System.out.println("The Number is Not an Armstrong Number");
+		}
+	}
 }
